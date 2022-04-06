c171ee12864e2c623e3c0aad5e86bb483406e0d8
MarioHM21 <98123147+MarioMH1@users.noreply.github.com>
2022-04-06 20:26:45 +0000

    Identacion correcta

--- sokoban.py
+++ sokoban.py
@@ -25,8 +25,8 @@ class sokoban:
             [4,1,1,1,1,1,1,1,4],
             [4,4,4,4,4,4,4,4,4]
         ]
-        self.perosnaje_fila = 3
-        self.personaje_columna = 5
+        self.personaje_fila = 2
+        self.personaje_columna = 3
 
     def imprimirmapa (self):
         for fila in self.mapa:
@@ -35,40 +35,17 @@ class sokoban:
     def moverderecha(self):
         print("mover derecha")
 
+        #Movimientos Derecha
+
         # 5. personaje, espacio
-        if (self.mapa[self.perosnaje_fila][self.personaje_columna] == self.personaje and              self.mapa[self.personaje_fila][self.personaje_columna + 1] ==self.espacio):
+        if (self.mapa[self.perosnaje_fila][self.personaje_columna] == 0  
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1):
 
-            self.mapa[self.perosnaje_fila][self.personaje_columna] = self.espacio
-            self.mapa[self.perosnaje_fila][self.personaje_columna + 1] = self.personaje
+            self.mapa[self.perosnaje_fila][self.personaje_columna] = 1
+            self.mapa[self.perosnaje_fila][self.personaje_columna + 1] =  0
             self.perosnaje_columna += 1
-        # 6. peronaje, meta
-        elif (self.mapa[self.personaje_fila][self.perosnaje_columna] == 0
-        and self.mapa[self.perosnaje_fila] [self.perosnaje_columna + 1] == 3):
-
-            self.mapa[self.perosnaje_fila][self.perosnaje_columna] = 1
-            self.mapa[self.perosnaje_fila][self.perosnaje_columna + 1] = 5
-            self.perosnaje_columna += 1
-        # 7. perosnaje, Caja, Espacio
-        elif (self.map[self.personaje_fila][self.perosnaje_columna] == 0
-        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
-        and self.mapa[self.perosnaje_fila][self.perosnaje_columna + 2] == 1):
-
-            self.mapa[self.personaje_fila][self.personaje_columna] = 1 #coloca un espacio donde estaba el personaje
-            self.mapa[self.perosnaje_fila][self.perosnaje_columna + 1] = 0 # coloca el perosnaje donde estaba la caja 
-            self.mapa[self.personaje_fila][self.perosnaje_columna + 2] = 2 # colocando la caja se encuentra donde estaba un espacio 
-            self.perosnaje_columna +=1
-        # 8. personaje , caja , meta
-        elif (self.mapa[self.personaje_fila][self.perosnaje_columna] == 0 
-        and self.mapa[self.perosnaje_fila][self.perosnaje_columna + 1] == 2 
-        and self.mapa[self.perosnaje_fila][self.personaje_columna + 2] == 3): 
-
-
-            self.mapa[self.perosnaje_fila][self.perosnaje_columna] = 1 # coloca un espacio dodne estaba el personaje
-            self.mapa[self.personaje_fila][self.perosnaje_columna + 1] = 0  # coloca el personaje donde esta la caja
-            self.mapa[self.perosnaje_fila][self.perosnaje_columna + 3] = 6 #coloca la caja donde esta la meta 
-
-            self.perosnaje_columna +=1
-    
+            print("personaje,espacio")
+      
 
       
 
@@ -87,19 +64,10 @@ class sokoban:
         self.leermapa()
         while True:
             self.imprimirmapa()
-           if.movimiento == input("movimiento")
+            movimiento = input("mover hacia:")
+            if movimiento == "d":
                 self.moverderecha()
-            elif.movimi == "d":
-                self.moverizquierda()
-            elif movimiento == "a":
-                self.movimientoarriba()
-            elif movimiento == "w":
-                self.movimientoabajo()
-            elif movimiento == "s":
-                self:movimientosalir
-                print("salir del juego")
-                break
-
+                
 
 juego = sokoban()
 juego.jugar()

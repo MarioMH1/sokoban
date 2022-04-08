f56c890c0d218431f9e43055006d5437c50296c8
MarioHM21 <98123147+MarioMH1@users.noreply.github.com>
2022-04-08 13:46:02 +0000

    avance derecha

--- sokoban.py
+++ sokoban.py
@@ -60,11 +60,89 @@ class sokoban:
         and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
         and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1):
 
-            self.mapa[self.personaje_fila][self.personaje_columna] =1
+            self.mapa[self.personaje_fila][self.personaje_columna] = 1
             self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
             self.mapa[self.personaje_fila][self.personaje_columna + 2] = 1
             self.personaje_columna += 1
             print("personaje,caja,espacio")
+
+        # 8. personaje, caja , meta
+        if (self.mapa[self.personaje_fila][self.personaje_columna] == 0
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
+        and self.mapa[self.personaje_fila][self.personaje_columna +  2] == 3):
+
+            self.mapa[self.personaje_fila][self.personaje_columna] = 1
+            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 3
+            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 1
+            self.personaje_columna += 1
+            print("personaje,caja,meta")
+
+
+        # 9. personaje ,caja_meta , espacio
+        if (self.mapa[self.personaje_fila][self.personaje_columna] == 0
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1):
+
+            self.mapa[self.personaje_fila][self.personaje_columna] = 1
+            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
+            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 1
+            self.personaje_columna *= 1
+            print("personaje , caja_meta, meta")
+
+        # 10. persoanje , caja_meta , meta
+        if (self.mapa[self.personaje_fila][self.personaje_columna] == 0
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 3):
+
+            self.mapa[self.personaje_fila][self.personaje_columna] = 1
+            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
+            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
+            self.personaje_columna += 1
+            print("personaje , caja_meta, meta")
+
+        # 11. personaje_meta, espacio
+        if (self.mapa[self.personaje.fila][self.personaje_columna] == 5
+        and self.mapa[self.persoanje_fila][self.persoanje_columna +1] == 1):
+
+            self.mapa[self.personaje_fila][self.personaje_columna] = 1
+            self.mapa[self.persoanje_fila][self.personaje_columna + 1] = 1
+            self.persoanje_columna += 1
+            print("persoanje_meta , espacio")
+
+        # 12.  persoanje_meta, meta
+        if (self.mapa[self.persoanje_fila][self.persoanje_columna] == 5
+        and self.mapa[self.persoanje_fila][self.persoanje_columna + 1] == 3):
+
+            self.mapa[self.personaje_fila][self.personaje_columna] = 3
+            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 3
+            self.persoanje_columna += 1
+            print("persoanje_meta, meta")
+
+        # 13. personaje_meta ,caja ,espacio
+        if (self.mapa[self.persoanje_fila][self.perosnaje_columna] == 5
+        and self.mapa[self.personaje_fila][self.persoanje_columna] == 2
+        and self.mapa[self.persoanje_fila][self.persoanje_columna + 1] == 1):
+
+            self.mapa[self.personaje_fila][self.persoanje_columna] = 1
+            self.mapa[self.personaje_fila][self.persoanje_columna] = 2
+            self.mapa[self.perosnaje_fila][self.persoanje_columna + 1] =1
+            self.persoanje_columna += 1
+            print("personaje_meta , caja ,espacio")
+
+        # 14 . personaje_meta , caja , meta
+
+            
+
+            
+            
+        
+        
+        
+
+
+
+    
+            
 
       
 
